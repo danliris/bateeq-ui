dbc6cb55c06cb86d88a86c93007199e3f7f25a07
lioenel <kepin91@yahoo.co.id>
2016-07-19 18:00:13 +0700

    Inventory
    Commit Inventory

---
+++ src/storage-inventory/list.js
@@ -0,0 +1,36 @@
+import {inject, Lazy} from 'aurelia-framework';
+import {Router} from 'aurelia-router';
+import {Service} from './service';
+
+
+@inject(Router, Service)
+export class List {
+    
+    constructor(router, service) {
+        this.router = router;
+        this.service = service;
+        this.storageId = "";
+        this.storages = [];
+        //this.data = {};
+    }
+
+    activate() { 
+        this.service.getAllStorage() 
+            .then(storages => {
+                this.storages = storages; 
+            })  
+    }
+    
+    reloadArticleVariant() { 
+        this.service.getAllInvetory(this.storageId) 
+            .then(data => {
+                this.data = data;  
+            })  
+    } 
+    
+    view(data) {
+        console.log(JSON.stringify(data));
+        this.router.navigateToRoute('view', { storageId: data.storageId, articleVariantId: data.articleVariantId, storageName: data.storage.name, articleVariantName: data.articleVariant.name });
+    }   
+    
+}
---
+++ src/storage-inventory/service.js
@@ -0,0 +1,33 @@
+import {inject, Lazy} from 'aurelia-framework';
+import {HttpClient} from 'aurelia-fetch-client';
+import {RestService} from '../rest-service';
+  
+const serviceUriStorages = 'http://bateeq-inventory-api.au-syd.mybluemix.net/v1/inventories/storages';
+const serviceUriInventories = 'http://bateeq-inventory-api.au-syd.mybluemix.net/v1/inventories/storages/{storageId}/inventories';
+const serviceUriMovements = 'http://bateeq-inventory-api.au-syd.mybluemix.net/v1/inventories/storages/{storageId}/inventories/{articleVariantId}/movements';
+
+export class Service extends RestService{
+  
+  constructor() {
+    super();
+  } 
+
+  getAllStorage()
+  {
+    var endpoint = `${serviceUriStorages}`;
+    return super.get(endpoint);
+  } 
+   
+  getAllInvetory(storageId)
+  {
+    var endpoint = `${serviceUriInventories.replace("{storageId}", storageId)}`;
+    return super.get(endpoint);
+  }
+  
+  getAllMovement(storageId, articleVariantId)
+  {
+    var endpoint = `${serviceUriMovements.replace("{storageId}", storageId).replace("{articleVariantId}", articleVariantId)}`;
+    return super.get(endpoint);
+  }
+   
+}
