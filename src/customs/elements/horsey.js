4fb07c13dd930ffd28b5ba437c1d7bc9f17fc51f
Tris Setiawan <tris.setiawan@yahoo.com>
2016-08-15 19:02:02 +0700

    update kb-fng + horsey

--- src/customs/elements/horsey.js
+++ src/customs/elements/horsey.js
@@ -8,51 +8,34 @@ export class Horsey {
 
     @bindable({ defaultBindingMode: bindingMode.twoWay }) selection;
     @bindable({ defaultBindingMode: bindingMode.twoWay }) value;
+    @bindable({ defaultBindingMode: bindingMode.twoWay }) map;
 
     @bindable options;
-
     constructor(element) {
         this.element = element;
     }
 
     attached() {
-        // console.log(this.options);
         this.selection = {};
         var uri = `${this.src}?${this.filter ? this.filter : 'keyword'}`;
         HorseyJs(this.element.querySelector('input'), {
             predictNextSearch: info => {
-                // let changeEvent;
-                // if (window.CustomEvent) {
-                //     changeEvent = new CustomEvent('ce', {
-                //         detail: {
-                //             value: info.selection
-                //         },
-                //         bubbles: true
-                //     });
-                // } else {
-                //     changeEvent = document.createEvent('CustomEvent');
-                //     changeEvent.initCustomEvent('ce', true, true, {
-                //         detail: {
-                //             value: info.selection
-                //         }
-                //     });
-                // }
-                // this.element.dispatchEvent(changeEvent); 
-                this.selection = info.selection;
-                this.value = info.selection[eval(this.options).value]
+                this.selection = this.options.selection ? info.selection[this.options.selection] : info.selection;
+                this.value = info.selection[this.options.value];
+                this.text = info.selection[this.options.label]
             },
             source: (data, done) => {
                 fetch(`${uri}=${data.input}`)
                     .then(result => {
                         result.json().then(json => {
                             done(null, [{
-                                list: json.data
+                                list: this.map ? this.map(json) : json.data
                             }])
                         })
                     });
             },
-            getText: eval(this.options).label,
-            getValue: eval(this.options).value
+            getText: this.options.label,
+            getValue: this.options.value
         })
     }
 }
