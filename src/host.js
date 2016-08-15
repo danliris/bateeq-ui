b53b70cf2255c116c18ae26f4e9b6cc294dc5811
lioenel <kepin91@yahoo.co.id>
2016-08-15 13:24:57 +0700

    change password type
    change password type

--- src/host.js
+++ src/host.js
@@ -1,5 +1,5 @@
 module.exports = {
     core: "http://bateeq-core-api.mybluemix.net/v1/core",
-    inventory: "http://bateeq-inventory-api.mybluemix.net/v1/inventory",
-    merchandiser:"http://bateeq-inventory-api.mybluemix.net/v1/merchandiser"
+    inventory: "http://localhost:9001/v1/inventory",
+    merchandiser:"http://localhost:9001/v1/merchandiser"
 }
