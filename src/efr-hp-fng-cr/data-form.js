08966d7c86e393fb338b5a29c3cb5a295b41222a
lioenel <kepin91@yahoo.co.id>
2016-08-19 17:08:02 +0700

    get source and destination from config FNG

--- src/efr-hp-fng-cr/data-form.js
+++ src/efr-hp-fng-cr/data-form.js
@@ -1,53 +1,88 @@
 import {inject, bindable} from 'aurelia-framework';
 import {Router} from 'aurelia-router';
 import {Service} from './service';
- 
+
 @inject(Router, Service)
 export class DataForm {
     @bindable data = {};
-    @bindable error = {}; 
-    
+    @bindable error = {};
+
     serviceUriStorages = require('../host').inventory + '/storages';
     variantApiUri = require('../host').core + '/articles/variants';
-    
-    constructor(router, service) { 
+
+    constructor(router, service) {
         this.router = router;
-        this.service = service;   
+        this.service = service;
+        this.service.getModuleConfig()
+            .then(config => {
+                var getStorages = [];
+                var indexSource = 0;
+                if (config.source.type == "Selection") {
+                    for (var sourceId of config.source.value) {
+                        getStorages.push(this.service.getStorageById(sourceId));
+                        indexSource++;
+                    }
+                }
+                else {
+                    getStorages.push(this.service.getStorageById(config.source.value));
+                    indexSource++;
+                }
+                if (config.destination.type == "Selection") {
+                    for (var destinationId of config.destination.value) {
+                        getStorages.push(this.service.getStorageById(destinationId));
+                    }
+                }
+                else {
+                    getStorages.push(this.service.getStorageById(config.destination.value));
+                }
+                Promise.all(getStorages)
+                    .then(storages => {
+                        this.sources = storages.splice(0, indexSource);
+                        this.data.sourceId = this.sources[0]._id;
+                        this.data.source = this.sources[0];
+                        this.destinations = storages.splice(0);
+                        this.data.destinationId = this.destinations[0]._id;
+                        this.data.destination = this.destinations[0];
+                    })
+            })
+            .catch(e => {
+                console.log(e)
+                this.loadFailed = true;
+            })
     }
 
-    getQuantity(item) { 
-        for(var finishing of item.articleVariant.finishings) {
+    getQuantity(item) {
+        for (var finishing of item.articleVariant.finishings) {
             finishing.quantity = item.quantity * finishing.quantityEach;
         }
     }
-    
-    attached() {     
-    }  
-    
-    addItem() {           
+
+    attached() {
+    }
+
+    addItem() {
         var item = {};
-        item.articleVariantId = ''; 
+        item.articleVariantId = '';
         item.articleVariant = { finishings: [] };
-        this.data.items.push(item); 
-    } 
-    
-    removeItem(item) { 
+        this.data.items.push(item);
+    }
+
+    removeItem(item) {
         var itemIndex = this.data.items.indexOf(item);
         this.data.items.splice(itemIndex, 1);
     }
-    
-    addItemDetail(index) {           
+
+    addItemDetail(index) {
         var item = {};
-        item.articleVariantId = ''; 
-        if(!this.data.items[index].articleVariant.finishings){ 
-            this.data.items[index].articleVariant.finishings= [];
-        }  
-        this.data.items[index].articleVariant.finishings.push(item); 
-    } 
-    
-    removeItemDetail(index, item) { 
+        item.articleVariantId = '';
+        if (!this.data.items[index].articleVariant.finishings) {
+            this.data.items[index].articleVariant.finishings = [];
+        }
+        this.data.items[index].articleVariant.finishings.push(item);
+    }
+
+    removeItemDetail(index, item) {
         var itemIndex = this.data.items[index].articleVariant.finishings.indexOf(item);
         this.data.items[index].articleVariant.finishings.splice(itemIndex, 1);
     }
 }
- 
