90b73e35d245324d790d80f2bf931a458be20d4f
JanuitaM <januita.marpaung@moonlay.com>
2016-08-23 10:04:05 +0700

    display source and destination in efr-kb-rtp

--- src/efr-kb-rtp-cr/data-form.js
+++ src/efr-kb-rtp-cr/data-form.js
@@ -12,7 +12,47 @@ export class DataForm {
     
     constructor(router, service) { 
         this.router = router;
-        this.service = service;  
+        this.service = service;
+         this.service.getModuleConfig()
+            .then(config => {
+                var getStorages = [];
+                var indexSource = 0;
+
+                if (config.source.type == "selection") {
+                    for (var sourceId of config.source.value) {
+                        getStorages.push(this.service.getStorageById(sourceId.toString()));
+                        indexSource++;
+                    }
+                }
+                else {
+                    getStorages.push(this.service.getStorageById(config.source.value.toString()));
+                    indexSource++
+                }
+
+                var getStoragesDestination = [];
+                if (config.destination.type == "selection") {
+                    for (var destinationId of config.destination.value) {
+                        getStorages.push(this.service.getStorageById(destinationId.toString()));
+                    }
+                }
+                else {
+                    getStorages.push(this.service.getStorageById(config.destination.value.toString()));
+                }
+
+                Promise.all(getStorages)
+                    .then(storages => {
+                        this.sources = storages.splice(0, indexSource);
+                        this.destinations = storages[0];
+                        this.data.sourceId = this.sources[0]._id;
+                        this.data.source = this.sources[0];
+                        this.data.destinationId = this.destinations._id;
+                        this.data.destination = this.destinations;
+                    })
+            })
+            .catch(e => {
+                console.log(e)
+                this.loadFailed = true;
+            })  
     }
      
     attached() {    
--- src/efr-kb-rtp-cr/service.js
+++ src/efr-kb-rtp-cr/service.js
@@ -32,4 +32,20 @@ export class Service extends RestService{
       var endpoint = `${serviceSearch}?keyword=${code}`;
       return super.get(endpoint);
   }  
+
+  getModuleConfig() {
+    var endpoint = require('../host').core + '/modules?keyword=EFR-KB/RTP';
+    return super.get(endpoint)
+      .then(results => {
+        if (results && results.length == 1)
+          return Promise.resolve(results[0].config);
+        else
+          return Promise.resolve(null);
+      });
+  }
+  
+  getStorageById(id) {
+    var endpoint = `${require('../host').inventory + '/storages'}/${id}`;
+    return super.get(endpoint);
+  }
 }
