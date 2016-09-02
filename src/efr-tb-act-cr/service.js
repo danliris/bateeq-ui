073fdbc10bb373869d1db7c287285404dcf5fb03
JanuitaM <januita.marpaung@moonlay.com>
2016-09-02 17:11:17 +0700

    change efr-tb-acc become efr-tb-act

---
+++ src/efr-tb-act-cr/service.js
@@ -0,0 +1,57 @@
+import {inject, Lazy} from 'aurelia-framework';
+import {HttpClient} from 'aurelia-fetch-client';
+import {RestService} from '../rest-service';
+ 
+const serviceUri = require('../host').inventory + '/docs/efr-tb-act';
+ 
+export class Service extends RestService{
+
+  constructor(http, aggregator) {
+      super(http, aggregator);
+  }
+
+  search(keyword) {
+      return super.get(serviceUri);
+  }
+
+  getById(id)
+  {
+      var endpoint = `${serviceUri}/${id}`;
+      return super.get(endpoint);
+  }
+
+  getOutByCode(code) 
+  {
+      var endpoint = `${serviceOutUri}?keyword=${code}`;
+      return super.get(endpoint);
+  }
+  
+  create(data)
+  {
+      var endpoint = `${serviceUri}`;
+      return super.post(endpoint, data);
+  } 
+
+   getModuleConfig() {
+    var endpoint = require('../host').core + '/modules?keyword=EFR-TB/ACT';
+    return super.get(endpoint)
+      .then(results => {
+        if (results && results.length == 1)
+          return Promise.resolve(results[0].config);
+        else
+          return Promise.resolve(null);
+      });
+  }
+  
+  getStorageById(id) {
+    var endpoint = `${require('../host').inventory + '/storages'}/${id}`;
+    return super.get(endpoint);
+  }
+  
+
+  getSupplierById(id) {
+    var endpoint = `${require('../host').core + '/suppliers'}/${id}`;
+    return super.get(endpoint);
+  }
+  
+}
