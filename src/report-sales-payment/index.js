172e90554004df65b9cb43808f569f5b3d4241c4
lioenel <kepin91@yahoo.co.id>
2016-11-01 16:19:14 +0700

    remove pos

--- src/report-sales-payment/index.js
+++
@@ -1,9 +0,0 @@
-export class Index {
-  configureRouter(config, router) {
-    config.map([
-        {route:['', '/list'],       moduleId:'./list',          name:'list',        nav:false,      title:'Report Sales Payment'},
-    ]);
-
-    this.router = router;
-  }
-}
