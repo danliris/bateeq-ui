b12fcf2cdf67dd30dd2bdf952e227d74a8cd9f14
Tris Setiawan <tris.setiawan@yahoo.com>
2016-11-16 18:53:20 +0700

    update host using https

--- src/host.js
+++ src/host.js
@@ -1,8 +1,8 @@
 module.exports = {
-    auth: "http://bateeq-auth-api-dev.mybluemix.net",
-    core: "http://bateeq-core-api-dev.mybluemix.net/v1/core",
-    inventory: "http://bateeq-inventory-api-dev.mybluemix.net/v1/inventory",
-    merchandiser: "http://bateeq-inventory-api-dev.mybluemix.net/v1/merchandiser",
-    master: "http://bateeq-inventory-api-dev.mybluemix.net/v1/master",
-    sales: "http://bateeq-pos-api-dev.mybluemix.net/v1/sales"
+    auth: "https://bateeq-auth-api-dev.mybluemix.net",
+    core: "https://bateeq-core-api-dev.mybluemix.net/v1/core",
+    inventory: "https://bateeq-inventory-api-dev.mybluemix.net/v1/inventory",
+    merchandiser: "https://bateeq-inventory-api-dev.mybluemix.net/v1/merchandiser",
+    master: "https://bateeq-inventory-api-dev.mybluemix.net/v1/master",
+    sales: "https://bateeq-pos-api-dev.mybluemix.net/v1/sales"
 }
