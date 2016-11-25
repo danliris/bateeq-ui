0fdcb9cf7a14f92242a8d2e70bb1dfc96167d789
JanuitaM <januita.marpaung@moonlay.com>
2016-11-26 00:07:03 +0700

    upload file csv pbj in upload-file pbj

---
+++ src/modules/upload-file-pbj/upload.js
@@ -0,0 +1,49 @@
+import { inject, Lazy } from 'aurelia-framework';
+import { Router } from 'aurelia-router';
+import { Service } from './service';
+
+
+@inject(Router, Service, Element)
+export class Create {
+
+    constructor(router, service, element) {
+        this.router = router;
+        this.service = service;
+        this.element = element;
+        this.data = { items: [] };
+    }
+
+    activate(params) {
+    }  
+
+    list()
+    {
+        this.router.navigateToRoute('list');
+    }
+
+    upload() {
+        var formData = new FormData();
+        var fileInput = document.getElementById("fileCsv");
+        var fileList = fileInput.files;
+        var source = this.data.source;
+        var destination = this.data.destination;
+        var date = this.data.date;
+        formData.append("sourceId", source._id);
+        formData.append("destinationId", destination._id);
+        formData.append("date", date);
+        formData.append("fileUpload", fileList[0]);
+
+        var uri = require('../../host').merchandiser + '/upload';;
+        var request = {
+            method: 'POST',
+            headers: {
+            },
+            body: formData
+        };
+        fetch(uri, request)
+            .then(response => {
+                alert("Data Berhasil Diupload");
+                this.router.navigateToRoute('list');
+            })
+    }
+}
