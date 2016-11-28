0404f8c6c92a00ce7bca2b390933dabf829d96d6
JanuitaM <januita.marpaung@moonlay.com>
2016-11-28 10:09:46 +0700

    change host.js

--- src/host.js
+++ src/host.js
@@ -1,11 +1,8 @@
 module.exports = {
     auth: "https://bateeq-auth-api-dev.mybluemix.net",
-   // core: "https://bateeq-core-api-dev.mybluemix.net/v1/core",
-    core: "http://127.0.0.1:9090/v1/master",
-   // inventory: "https://bateeq-inventory-api-dev.mybluemix.net/v1/inventory",
-    inventory: "http://127.0.0.1:9070/v1/inventory",
-    merchandiser: "http://127.0.0.1:9070/v1/merchandiser",
-    //merchandiser: "https://bateeq-inventory-api-dev.mybluemix.net/v1/merchandiser",
+    core: "https://bateeq-core-api-dev.mybluemix.net/v1/core",
+    inventory: "https://bateeq-inventory-api-dev.mybluemix.net/v1/inventory",
+    merchandiser: "https://bateeq-inventory-api-dev.mybluemix.net/v1/merchandiser",
     master: "https://bateeq-inventory-api-dev.mybluemix.net/v1/master",
     sales: "https://bateeq-pos-api-dev.mybluemix.net/v1/sales"
 }
