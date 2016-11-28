e4e967ae0ae4e2e293d7c4e5c85a5d0b774b2fa8
JanuitaM <januita.marpaung@moonlay.com>
2016-11-28 10:39:10 +0700

    upload csv file pbj and pba file (#32)
    * change menu in bateeq-ui
    
    * change article become item in efr-tb-bat-cr
    
    * change article become item in efr-tb-bbt-cr
    
    * upload file csv pbj in upload-file pbj
    
    * upload file csv pba in upload-file-pba
    
    * change host.js

---
+++ src/modules/upload-file-pbj/index.js
@@ -0,0 +1,9 @@
+export class Index {
+  configureRouter(config, router) {
+    config.map([
+      { route: ['', '/list'], moduleId: './list',   name: 'list',   nav: false, title: 'Upload File PBJ' },
+      { route: 'upload',      moduleId: './upload', name: 'upload', nav: false, title: 'Upload:Upload File PBJ' }
+    ]);
+    this.router = router;
+  }
+}
