9ac4a1b508e02dd194ad8cee00d121bed89a228d
fendywillyam <fendy.willyam@moonlay.com>
2016-12-22 10:55:37 +0700

    Update host.js

--- src/host.js
+++ src/host.js
@@ -1,12 +1,12 @@
 module.exports = {
     //UAT
-    auth: "https://bateeq-auth-api-uat.mybluemix.net",
-    core: "https://bateeq-core-api-uat.mybluemix.net/v1/core",
-    master: "https://bateeq-core-api-uat.mybluemix.net/v1/master",  
-    inventory: "https://bateeq-inventory-api-uat.mybluemix.net/v1/inventory",
-    merchandiser: "https://bateeq-inventory-api-uat.mybluemix.net/v1/merchandiser",
-     sales: "https://bateeq-pos-api-uat.mybluemix.net/v1/sales"
-    // master: "https://bateeq-pos-api-dev.mybluemix.net/v1/master",
+    //auth: "https://bateeq-auth-api-uat.mybluemix.net",
+    //core: "https://bateeq-core-api-uat.mybluemix.net/v1/core",
+    //master: "https://bateeq-core-api-uat.mybluemix.net/v1/master",  
+   // inventory: "https://bateeq-inventory-api-uat.mybluemix.net/v1/inventory",
+    //merchandiser: "https://bateeq-inventory-api-uat.mybluemix.net/v1/merchandiser",
+     //sales: "https://bateeq-pos-api-uat.mybluemix.net/v1/sales"
+ 
    
     // inventory: "http://localhost:9070/v1/inventory",
     // merchandiser:"http://localhost:9070/v1/merchandiser",
