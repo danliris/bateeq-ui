804281145a5a6da9a66399fc0aa1f35cba1d7d68
JanuitaM <januita.marpaung@moonlay.com>
2017-01-18 19:07:14 +0700

    change data-form

--- src/app.js
+++ src/app.js
@@ -20,10 +20,10 @@ export class App {
       { route: 'efr-tb-bbt', name: 'efr-tb-bbt', moduleId: './modules/efr-tb-bbt-cr/index', nav: true, title: 'Pemasukan Barang Jadi', auth: true, settings: { group: "toko", roles: ["admin"] } },
       { route: 'efr-tb-bat', name: 'efr-tb-bat', moduleId: './modules/efr-tb-bat-cr/index', nav: true, title: 'Pemasukan Embalase', auth: true, settings: { group: "toko", roles: ["admin"] } },
       { route: 'efr-kb-rtp', name: 'efr-kb-rtp', moduleId: './modules/efr-kb-rtp-cr/index', nav: true, title: 'Retur Barang Ke Pusat', auth: true, settings: { group: "toko", roles: ["admin"] } },
-      { route: 'power-bi', name: 'power-bi', moduleId: './modules/power-bi/index', nav: true, title: 'Power BI Reports', auth: true, settings: { group: "dashboard", roles: ["admin"] } },
-      { route: 'upload-file-pba', name: 'upload-file-pba', moduleId: './modules/upload-file-pba/index', nav: true, title: 'Upload Packing List Embalase ke Toko', auth: true, settings: { group: "gudang pusat", roles: ["admin"] } },
       { route: 'upload-file-pbj', name: 'upload-file-pbj', moduleId: './modules/upload-file-pbj/index', nav: true, title: 'Upload Packing List Barang Jadi ke Toko', auth: true, settings: { group: "gudang pusat", roles: ["admin"] } },
-      { route: 'inventories', name: 'inventories', moduleId: './modules/storage-inventory/index', nav: true, title: 'Laporan Stok', auth: true, settings: { group: "laporan", roles: ["admin"] } }
+      { route: 'upload-file-pba', name: 'upload-file-pba', moduleId: './modules/upload-file-pba/index', nav: true, title: 'Upload Packing List Embalase ke Toko', auth: true, settings: { group: "gudang pusat", roles: ["admin"] } },
+      { route: 'inventories', name: 'inventories', moduleId: './modules/storage-inventory/index', nav: true, title: 'Laporan Stok', auth: true, settings: { group: "laporan", roles: ["admin"] } },
+      { route: 'power-bi', name: 'power-bi', moduleId: './modules/power-bi/index', nav: true, title: 'Power BI Reports', auth: true, settings: { group: "dashboard", roles: ["admin"] } }
     ];
 
     config.map(routes);
