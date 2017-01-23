3479511b1eeaae88929b4bd32aa87bc7e11412fb
danielmoonlay <daniel.nababan@moonlay.com>
2017-01-23 13:56:27 +0700

    add report daily
    1. report daily store
    2. report daily by sales Category
    3. menu minimazed default

--- src/app.js
+++ src/app.js
@@ -23,7 +23,9 @@ export class App {
       { route: 'upload-file-pbj', name: 'upload-file-pbj', moduleId: './modules/upload-file-pbj/index', nav: true, title: 'Upload Packing List Barang Jadi ke Toko', auth: true, settings: { group: "gudang pusat", roles: ["admin"] } },
       { route: 'upload-file-pba', name: 'upload-file-pba', moduleId: './modules/upload-file-pba/index', nav: true, title: 'Upload Packing List Embalase ke Toko', auth: true, settings: { group: "gudang pusat", roles: ["admin"] } },
       { route: 'inventories', name: 'inventories', moduleId: './modules/storage-inventory/index', nav: true, title: 'Laporan Stok', auth: true, settings: { group: "laporan", roles: ["admin"] } },
-      { route: 'power-bi', name: 'power-bi', moduleId: './modules/power-bi/index', nav: true, title: 'Power BI Reports', auth: true, settings: { group: "dashboard", roles: ["admin"] } }
+      { route: 'power-bi', name: 'power-bi', moduleId: './modules/power-bi/index', nav: true, title: 'Power BI Reports', auth: true, settings: { group: "dashboard", roles: ["admin"] } },
+      { route: 'daily-omzet-store', name: 'daily-omzet-store', moduleId: './modules/daily-omzet-store/index', nav: true, title: 'Omset Harian (per Toko)', auth: true, settings: { group: "dashboard", roles: ["admin"] } },
+      { route: 'daily-omzet-sales-category', name: 'daily-omzet-sales-category', moduleId: './modules/daily-omzet-sales-category/index', nav: true, title: 'Omset Harian (per Penjualan)', auth: true, settings: { group: "dashboard", roles: ["admin"] } }
     ];
 
     config.map(routes);
