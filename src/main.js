dc24872ce57a107be3da32f76b37bf72d0d6d56d
fendywillyam <fendy.willyam@moonlay.com>
2017-03-09 11:46:10 +0700

    Update main.js

--- src/main.js
+++ src/main.js
@@ -20,12 +20,12 @@ export async function configure(aurelia) {
     .feature('converters')
     .plugin("aurelia-api", config => {
     //DEV
-      var auth = "https://bateeq-auth-api-dev.mybluemix.net/v1/";
-      var core = "https://bateeq-core-api-dev.mybluemix.net/v1/core/";
-      var master = "https://bateeq-core-api-dev.mybluemix.net/v1/master/";
-      var inventory = "https://bateeq-inventory-api-dev.mybluemix.net/v1/inventory/";
-      var merchandiser = "https://bateeq-inventory-api-dev.mybluemix.net/v1/merchandiser/";
-      var sales = "https://bateeq-pos-api-dev.mybluemix.net/v1/sales/";
+      var auth = "https://bateeq-auth-api-uat.mybluemix.net/v1/";
+      var core = "https://bateeq-core-api-uat.mybluemix.net/v1/core/";
+      var master = "https://bateeq-core-api-uat.mybluemix.net/v1/master/";
+      var inventory = "https://bateeq-inventory-api-uat.mybluemix.net/v1/inventory/";
+      var merchandiser = "https://bateeq-inventory-api-uat.mybluemix.net/v1/merchandiser/";
+      var sales = "https://bateeq-pos-api-uat.mybluemix.net/v1/sales/";
       //var auth = "https://bateeq-auth-api.mybluemix.net/v1/";
       //var core = "https://bateeq-core-api.mybluemix.net/v1/core/";
       //var master = "https://bateeq-core-api.mybluemix.net/v1/master/";
