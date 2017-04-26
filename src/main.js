b35414733a8015651d20492d8fedae97e6d2e631
fendywillyam <fendy.willyam@moonlay.com>
2017-04-26 16:29:41 +0700

    Update main.js

--- src/main.js
+++ src/main.js
@@ -22,18 +22,13 @@ export async function configure(aurelia) {
     .plugin("aurelia-api", config => {
     //DEV
 
-      //var auth = "https://bateeq-auth-api-uat.mybluemix.net/v1/";
-      //var core = "https://bateeq-core-api-uat.mybluemix.net/v1/core/";
-      //var master = "https://bateeq-core-api-uat.mybluemix.net/v1/master/";
-     // var inventory = "https://bateeq-inventory-api-uat.mybluemix.net/v1/inventory/";
-     // var merchandiser = "https://bateeq-inventory-api-uat.mybluemix.net/v1/merchandiser/";
-     // var sales = "https://bateeq-pos-api-uat.mybluemix.net/v1/sales/";
-      var auth = "https://bateeq-auth-api.mybluemix.net/v1/";
-      var core = "https://bateeq-core-api.mybluemix.net/v1/core/";
-      var master = "https://bateeq-core-api.mybluemix.net/v1/master/";
-      var inventory = "https://bateeq-inventory-api.mybluemix.net/v1/inventory/";
-      var merchandiser = "https://bateeq-inventory-api.mybluemix.net/v1/merchandiser/";
-      var sales = "https://bateeq-pos-api.mybluemix.net/v1/sales/";
+      var auth = "https://bateeq-auth-api-uat.mybluemix.net/v1/";
+      var core = "https://bateeq-core-api-uat.mybluemix.net/v1/core/";
+      var master = "https://bateeq-core-api-uat.mybluemix.net/v1/master/";
+      var inventory = "https://bateeq-inventory-api-uat.mybluemix.net/v1/inventory/";
+      var merchandiser = "https://bateeq-inventory-api-uat.mybluemix.net/v1/merchandiser/";
+      var sales = "https://bateeq-pos-api-uat.mybluemix.net/v1/sales/";
+
 
       
       config.registerEndpoint('auth', auth);
