ebac795f300ba140d0cfcd1449f06d762670cb1b
SirDimas <dimassixer@gmail.com>
2017-06-16 10:46:45 +0700

    Stock Opname

---
+++ src/modules/stock-opname/upload.js
@@ -0,0 +1,75 @@
+import { inject, Lazy } from 'aurelia-framework';
+import { Router } from 'aurelia-router';
+import { Service } from './service';
+var StorageLoader = require('../../loader/storage-loader');
+
+
+@inject(Router, Service, Element)
+export class Create {
+
+    constructor(router, service) {
+        this.router = router;
+        this.service = service;
+        this.data = { items: [] };
+    }
+
+    activate(params) {
+    }
+
+    list() {
+        this.router.navigateToRoute('list');
+    }
+
+    get storageLoader() {
+        return StorageLoader;
+    }
+
+    upload() {
+        var e = {};
+        var formData = new FormData();
+        var fileInput = document.getElementById("fileCsv");
+        var fileList = fileInput.files;
+        var storage = this.data.storage;
+        
+        if (fileList[0] == undefined) {
+            e.file = "File Path harus dipilih";
+            this.error = e;
+        } else if(!storage){
+            e.storage = "Harus diisi";
+            this.error = e;
+        }  else {
+            formData.append("storageId", storage._id);
+            formData.append("fileUpload", fileList[0]);
+
+            var endpoint = 'stock-opnames';
+            var request = {
+                method: 'POST',
+                headers: {
+                },
+                body: formData
+            };
+            var promise = this.service.endpoint.client.fetch(endpoint, request)
+            this.service.publish(promise);
+            return promise
+                .then((result) => {
+                    this.service.publish(promise);
+                    if (result.status == 200) {
+                        var getRequest = this.service.endpoint.client.fetch(endpoint, request);
+                        this.service._downloadFile(getRequest);
+                        this.service.publish(getRequest);
+                        alert("Upload gagal!\n Ada beberapa data yang harus diperbaiki. Silahkan lihat Error Log untuk melihat detil dari error tersebut.");
+                        this.list();
+                    }
+                    else if (result.status == 404) {
+                        alert("Urutan format kolom CSV tidak sesuai.\n Format: Barcode, Nama Barang, Kuantitas Stock");
+                    }
+                    else {
+                        alert("Data Berhasil Diupload");
+                        this.list();
+
+                    }
+                    return Promise.resolve(result);
+                });
+        }
+    }
+}
--- src/routes/general-inventory.js
+++ src/routes/general-inventory.js
@@ -11,5 +11,19 @@ module.exports = [
             permission: { "FA.01": 1, "FA.02": 1, "GDG.01": 1, "GDG.02": 1, "GDG.03": 1, "GDG.04": 1, "GDG.05": 1, "GDG.10": 1 },
             iconClass: 'fa fa-gg-circle'
         }
+    },
+    
+    {
+        route: 'stock-opname',
+        name: 'stock-opname',
+        moduleId: './modules/stock-opname/index',
+        nav: true,
+        title: 'Stock Opname',
+        auth: true,
+        settings: {
+            group: "general inventory",
+            permission: { "FA.01": 1, "FA.02": 1, "GDG.01": 1, "GDG.02": 1, "GDG.03": 1, "GDG.04": 1, "GDG.05": 1, "GDG.10": 1 },
+            iconClass: 'fa fa-gg-circle'
+        }
     }
 ]
