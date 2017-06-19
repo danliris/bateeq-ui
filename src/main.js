8015e76d75c36c59e9231ca49fc4eafb68128e0d
andibateeq <andibateeq@gmail.com>
2017-06-19 15:43:02 +0700

    ReportAdjustment

--- src/main.js
+++ src/main.js
@@ -24,7 +24,8 @@ export async function configure(aurelia) {
       var auth = "https://bateeq-auth-api-dev.mybluemix.net/v1/";
       var core = "https://bateeq-core-api-dev.mybluemix.net/v1/core/";
       var master = "https://bateeq-core-api-dev.mybluemix.net/v1/master/";
-      var inventory = "https://bateeq-inventory-api-dev.mybluemix.net/v1/inventory/";
+    //var inventory = "https://bateeq-inventory-api-dev.mybluemix.net/v1/inventory/";
+     // var inventory = "http://127.0.0.1:9070/v1/inventory/";
       var merchandiser = "https://bateeq-inventory-api-dev.mybluemix.net/v1/merchandiser/";
       var sales = "https://bateeq-pos-api-dev.mybluemix.net/v1/sales/";
       //var auth = "https://bateeq-auth-api.mybluemix.net/v1/";
