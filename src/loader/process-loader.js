6e7aabbd1b32bf6320335958606d7abd08770cf6
JanuitaM <januita.marpaung@moonlay.com>
2017-06-20 11:32:41 +0700

    update form upload image product

--- src/loader/process-loader.js
+++ src/loader/process-loader.js
@@ -1,12 +1,12 @@
 import { Container } from 'aurelia-dependency-injection';
 import { Config } from "aurelia-api";
 
-const resource = 'storages';
+const resource = 'articles/process';
 
 module.exports = function (keyword, filter) {
 
     var config = Container.instance.get(Config);
-    var endpoint = config.getEndpoint("master");
+    var endpoint = config.getEndpoint("core");
     
     return endpoint.find(resource, { keyword: keyword })
         .then(results => {
