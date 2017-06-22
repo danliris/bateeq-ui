63b72d9580d89c721d6fe219b58747cd26b9fea9
achensetyawati <achenzetya@gmail.com>
2017-06-22 13:38:35 +0700

    product list

--- src/modules/merchandiser/product/list.js
+++ src/modules/merchandiser/product/list.js
@@ -1,44 +1,67 @@
-import { inject } from 'aurelia-framework';
-import { Service } from "./service";
+import { inject, Lazy} from 'aurelia-framework';
 import { Router } from 'aurelia-router';
+import { Service } from './service';
+var moment = require("moment");
+var locale = 'id-ID';
 
 @inject(Router, Service)
 export class List {
+    context = ['detail'];
     data = [];
+    info = { page: 1, keyword: '' };
+    keyword = '';
 
     constructor(router, service) {
-        this.service = service;
         this.router = router;
-    }
-    activate() {
-        this.service.search('')
-            .then(data => {
-                this.data = data.data;
-            })
+        this.service = service;
     }
 
-    // async activate() {
-    //     this.info.keyword = '';
-    //     var result = await this.service.search(this.info);
-    //     this.data = result.data;
-    //     this.info = result.info;
-    // }
-
-    loadPage() {
-        var keyword = this.info.keyword;
-        this.service.search(this.info)
-            .then(result => {
-                this.data = result.data;
-                // this.info = result.info;
-                // this.info.keyword = keyword;
-            })
+    create() {
+        this.router.navigateToRoute('create');
     }
 
-    // changePage(e) {
-    //     var page = e.detail;
-    //     this.info.page = page;
-    //     this.loadPage();
-    // }
+    columns = [
+            { field: "code", title: "Kode Barang" },
+            { field: "name", title: "Nama Barang" },
+            { field: "article.realizationOrder", title: "RO" }
+        ];
+    
+
+    loader = (info) => {
+        var order = {};
+        
+        if (info.sort) {
+            order[info.sort] = info.order;
+        }
+
+        var arg = {
+            page: parseInt(info.offset / info.limit, 10) + 1,
+            size: info.limit,
+            keyword: info.search,
+            order: order
+        };
+
+        return this.service.search(arg)
+            .then(result =>{
+                var data = {};
+                data.total = result.info.total;
+                data.data = result.data;
+                return {
+                    total: result.info.total,
+                    data: result.data
+                }
+            });
+        
+    };
+
+    contextCallback(event) {
+        var arg = event.detail;
+        var data = arg.data;
+        switch (arg.name) {
+            case "detail":
+                this.router.navigateToRoute('view', { id: data._id })
+        }
+    }
 
     view(data) {
         this.router.navigateToRoute('view', { id: data._id });
@@ -51,6 +74,5 @@ export class List {
     uploadImage() {
         this.router.navigateToRoute('upload-image');
     }
-
-
 }
+
