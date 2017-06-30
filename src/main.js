d6031d110adbe63ccf360465fecf1bfb775a4a9a
citraly <citra.sari@moonlay.com>
2017-06-30 14:18:24 +0700

    Update main.js

--- src/main.js
+++ src/main.js
@@ -24,20 +24,20 @@ export async function configure(aurelia) {
 
 
 
-      var auth = "https://bateeq-auth-api-dev.mybluemix.net/v1/";
-      var core = "https://bateeq-core-api-dev.mybluemix.net/v1/core/";
-      var master = "https://bateeq-core-api-dev.mybluemix.net/v1/master/";
-      var inventory = "https://bateeq-inventory-api-dev.mybluemix.net/v1/inventory/";
-      var merchandiser = "https://bateeq-inventory-api-dev.mybluemix.net/v1/merchandiser/";
-      var sales = "https://bateeq-pos-api-dev.mybluemix.net/v1/sales/";
+      //var auth = "https://bateeq-auth-api-dev.mybluemix.net/v1/";
+      //var core = "https://bateeq-core-api-dev.mybluemix.net/v1/core/";
+      //var master = "https://bateeq-core-api-dev.mybluemix.net/v1/master/";
+      //var inventory = "https://bateeq-inventory-api-dev.mybluemix.net/v1/inventory/";
+      //var merchandiser = "https://bateeq-inventory-api-dev.mybluemix.net/v1/merchandiser/";
+      //var sales = "https://bateeq-pos-api-dev.mybluemix.net/v1/sales/";
 
 
-     // var auth = "https://bateeq-auth-api-uat.mybluemix.net/v1/";
-     // var core = "https://bateeq-core-api-uat.mybluemix.net/v1/core/";
-     // var master = "https://bateeq-core-api-uat.mybluemix.net/v1/master/";
-     // var inventory = "https://bateeq-inventory-api-uat.mybluemix.net/v1/inventory/";
-     // var merchandiser = "https://bateeq-inventory-api-uat.mybluemix.net/v1/merchandiser/";
-     // var sales = "https://bateeq-pos-api-uat.mybluemix.net/v1/sales/";
+     var auth = "https://bateeq-auth-api-uat.mybluemix.net/v1/";
+     var core = "https://bateeq-core-api-uat.mybluemix.net/v1/core/";
+     var master = "https://bateeq-core-api-uat.mybluemix.net/v1/master/";
+     var inventory = "https://bateeq-inventory-api-uat.mybluemix.net/v1/inventory/";
+     var merchandiser = "https://bateeq-inventory-api-uat.mybluemix.net/v1/merchandiser/";
+     var sales = "https://bateeq-pos-api-uat.mybluemix.net/v1/sales/";
 
 
       
