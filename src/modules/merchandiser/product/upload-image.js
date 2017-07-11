2496ed36e35265dd33d158f113548ce7cd257c90
JanuitaM <januita.marpaung@moonlay.com>
2017-07-11 16:11:54 +0700

    add field category and remove motifDoc and motifpath

--- src/modules/merchandiser/product/upload-image.js
+++ src/modules/merchandiser/product/upload-image.js
@@ -5,21 +5,22 @@ var FinishedGoodsLoader = require('../../../loader/finished-goods-loader');
 var CountersLoader = require('../../../loader/counter-loader');
 var MaterialCompositionsLoader = require('../../../loader/material-composition-loader');
 var MaterialsLoader = require('../../../loader/material-loader');
-var MotifsLoader = require('../../../loader/motif-loader');
+// var MotifsLoader = require('../../../loader/motif-loader');
 var ProcessLoader = require('../../../loader/process-loader');
 var SeasonsLoader = require('../../../loader/season-loader');
 var SubCountersLoader = require('../../../loader/sub-counter-loader');
 var CollectionsLoader = require('../../../loader/collection-loader');
+var CategoriesLoader = require('../../../loader/category-loader');
 
 @inject(Router, Service)
 export class Upload {
-    motif={};
-    process={};
-    material={};
-    collection={};
-    season={};
-    counter={};
-    subCounter={};
+    motif = {};
+    process = {};
+    material = {};
+    collection = {};
+    season = {};
+    counter = {};
+    subCounter = {};
     @bindable data;
     @bindable error;
     contacts = [];
@@ -28,6 +29,8 @@ export class Upload {
     dataSource = [];
     dataDestination = [];
     // article_motif = {};
+    // article_category = {};
+    article_collection = {};
     article_colors = [];
     article_color = {};
     color = "#FF0000";
@@ -58,7 +61,7 @@ export class Upload {
                 });
             })
 
-        this.product.toString = function(){
+        this.product.toString = function () {
             return this.name || "";
         }
     }
@@ -88,6 +91,11 @@ export class Upload {
         return SeasonsLoader;
     }
 
+    get CategoriesLoader() {
+        return CategoriesLoader;
+    }
+
+
     controlOptions = {
         label: {
             length: 2,
@@ -110,6 +118,7 @@ export class Upload {
                     this.dataSource = result;
                     if (result) {
                         var firstResult = result[0];
+                        this.data.ro = this.ro;
                         // this.service.getMotif(firstResult.code.substring(9, 11))
                         //     .then((motif) => {
                         //         if (motif) {
@@ -177,13 +186,13 @@ export class Upload {
         this.dataSource = [].concat(temp);
     }
 
-    onClickAllDataSource($event){
-        for(var item of this.dataSource){
+    onClickAllDataSource($event) {
+        for (var item of this.dataSource) {
             item.check = $event.detail.target.checked;
         }
     }
-    onClickAllDataDestination($event){
-        for(var item of this.dataDestination){
+    onClickAllDataDestination($event) {
+        for (var item of this.dataDestination) {
             item.check = $event.detail.target.checked;
         }
     }
@@ -230,14 +239,14 @@ export class Upload {
         var imageUpload = document.getElementById("imageUpload");
         var fileList1 = imageUpload.files;
 
-        var motifUpload = document.getElementById("motifUpload");
-        var fileList2 = motifUpload.files;
+        // var motifUpload = document.getElementById("motifUpload");
+        // var fileList2 = motifUpload.files;
 
         if (fileList1[0] == undefined)
             e["imageUpload"] = "Gambar harus dipilih"
 
-        if (fileList2[0] == undefined)
-            e["motifUpload"] = "Gambar motif harus dipilih"
+        // if (fileList2[0] == undefined)
+        //     e["motifUpload"] = "Gambar motif harus dipilih"
 
         if (this.dataDestination.length == 0) {
             e["dataDestination"] = "Produk harus dipilih"
@@ -245,42 +254,39 @@ export class Upload {
                 e["dataSource"] = "Tidak ada produk ditemukan"
             }
         }
-        if (this.data.realizationOrderName=="" || this.data.realizationOrderName==undefined)
-        {
-             e["realizationOrderName"] = "Nama RO harus diisi"
+        // if (this.data.realizationOrderName == "" || this.data.realizationOrderName == undefined) {
+        //     e["realizationOrderName"] = "Nama RO harus diisi"
+        // }
+        if (this.data.process == "" || this.data.process == undefined) {
+            e["process"] = "Process harus diisi"
         }
-        if (this.data.process=="" || this.data.process==undefined)
-        {
-             e["process"] = "Process harus diisi"
+        if (this.data.materials == "" || this.data.materials == undefined) {
+            e["materials"] = "Bahan harus diisi"
         }
-        if (this.data.materials=="" || this.data.materials==undefined)
-        {
-             e["materials"] = "Bahan harus diisi"
+        if (this.data.materialCompositions == "" || this.data.materialCompositions == undefined) {
+            e["materialCompositions"] = "Komposisi Bahan harus diisi"
         }
-        if (this.data.materialCompositions=="" || this.data.materialCompositions==undefined)
-        {
-             e["materialCompositions"] = "Komposisi Bahan harus diisi"
+        if (this.data.collections == "" || this.data.collections == undefined) {
+            e["collections"] = "Koleksi harus diisi"
         }
-        if (this.data.collections=="" || this.data.collections==undefined)
-        {
-             e["collections"] = "Koleksi harus diisi"
+        if (this.data.counters == "" || this.data.counters == undefined) {
+            e["counters"] = "Season harus diisi"
         }
-        if (this.data.counters=="" || this.data.counters==undefined)
-        {
-             e["counters"] = "Season harus diisi" 
+        if (this.data.subCounters == "" || this.data.subCounters == undefined) {
+            e["subCounters"] = "Style harus diisi"
         }
-        if (this.data.subCounters=="" || this.data.subCounters==undefined)
-        {
-             e["subCounters"] = "Style harus diisi"
-        } 
-        if (this.data.seasons=="" || this.data.seasons==undefined)
-        {
-             e["seasons"] = "Season harus diisi"
-        } 
-        if (this.data.motif=="" || this.data.motif==undefined)
-        {
-             e["motif"] = "Nama harus diisi"
-        } 
+        if (this.data.seasons == "" || this.data.seasons == undefined) {
+            e["seasons"] = "Season harus diisi"
+        }
+        // if (this.data.motif == "" || this.data.motif == undefined) {
+        //     e["motif"] = "Nama harus diisi"
+        // }
+        if (this.data.categories == "" || this.data.categories == undefined) {
+            e["categories"] = "Kategori harus diisi"
+        }
+        if (this.data.ro == "" || this.data.ro == undefined) {
+            e["ro"] = "Nomor RO harus diisi"
+        }
 
         debugger
         if (Object.keys(e).length > 0) {
@@ -288,7 +294,7 @@ export class Upload {
         } else {
             var formData = new FormData();
             formData.append("imageUpload", fileList1[0]);
-            formData.append("motifUpload", fileList2[0]);
+            // formData.append("motifUpload", fileList2[0]);
 
             // console.log(formData);
             var endpoint = 'upload/image';
@@ -315,16 +321,18 @@ export class Upload {
                         data.colorCode = this.color;
                         data.articleColor = this.article_color;
                         data.imagePath = result.data[0];
-                        data.motifPath = result.data[1];
-                        data.realizationOrderName= this.data.realizationOrderName;
-                        data.processDoc=this.data.process;
-                        data.motifDoc=this.data.motif;
-                        data.seasonDoc=this.data.seasons;
-                        data.materialDoc=this.data.materials;
-                        data.materialCompositionDoc=this.data.materialCompositions;
-                        data.collectionDoc=this.data.collections;
-                        data.counterDoc=this.data.counters;
-                        data.styleDoc=this.data.subCounters;
+                        // data.motifPath = result.data[1];
+                        // data.realizationOrderName = this.data.realizationOrderName;
+                        data.processDoc = this.data.process;
+                        // data.motifDoc = this.data.motif;
+                        data.seasonDoc = this.data.seasons;
+                        data.materialDoc = this.data.materials;
+                        data.materialCompositionDoc = this.data.materialCompositions;
+                        data.collectionDoc = this.data.collections;
+                        data.counterDoc = this.data.counters;
+                        data.styleDoc = this.data.subCounters;
+                        data.categoryDoc = this.data.categories;
+                        data.ro = this.data.ro;
                         this.service.updateProductImage(data)
                             .then(result2 => {
                                 this.list();
