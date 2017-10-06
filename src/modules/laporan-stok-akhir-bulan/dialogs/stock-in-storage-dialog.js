383bf429c60c1dd4d9c415a79365b894440896ab
stvyang-m <steven.yang@moonlay.com>
2017-10-06 20:41:05 +0700

    Laporan stok akhir bulan

---
+++ src/modules/laporan-stok-akhir-bulan/dialogs/stock-in-storage-dialog.js
@@ -0,0 +1,39 @@
+import { inject, Lazy } from 'aurelia-framework';
+import { Service } from '.././service';
+import { DialogController } from 'aurelia-dialog';
+
+@inject(Service, DialogController)
+export class StockInStorageDialog {
+
+    data = {};
+    dialogTableData = [];
+
+    constructor(service, dialogController) {
+        this.service = service;
+        this.dialogController = dialogController;
+    }
+
+    activate(data, context) {
+        this.context = context;
+        this.data = data;
+        this.showTable(data);
+    }
+
+    dialogTableColumns = [
+        { field: 'number', title: 'No'},
+        { field: 'itemCode', title: 'Barcode'},
+        { field: 'itemName', title: 'Nama Barang'},
+        { field: 'quantity', title: 'Kuantitas'},
+        { field: 'totalHPP', title: 'Total HPP'},
+        { field: 'totalSale', title: 'Total Harga Jual'},
+    ]
+
+    showTable(result) {
+        let number = 0;
+        result.items.forEach(item => {
+            item.number = ++number;
+            this.dialogTableData.push(item)
+        })
+    }
+
+}   
--- src/modules/laporan-stok-akhir-bulan/index.js
+++ src/modules/laporan-stok-akhir-bulan/index.js
@@ -1,7 +1,7 @@
 export class Index {
     configureRouter(config, router) {
         config.map([
-            { route: ['', '/report'], moduleId: './report', name: 'report', nav: false, title: 'Omset Harian' }
+            { route: ['', '/report'], moduleId: './report', name: 'report', nav: false, title: 'Laporan Stok Akhir Bulan' }
         ]);
         this.router = router;
     }
