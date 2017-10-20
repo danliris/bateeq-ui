fa9ef266181df550d03f570c72d2e34185ceab35
stvyang <stv.yang10@gmail.com>
2017-10-20 19:56:37 +0700

    Change endpoint name

--- src/modules/laporan-stok-akhir-bulan/service.js
+++ src/modules/laporan-stok-akhir-bulan/service.js
@@ -4,7 +4,7 @@ import { RestService } from '../../utils/rest-service';
 import { Container } from 'aurelia-dependency-injection';
 import { Config } from "aurelia-api";
 
-const serviceUri = "monthly-stocks";
+const serviceUri = "monthly-stock";
 
 export class Service extends RestService {
 
