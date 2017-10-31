be50131043e1ebb756635bc9a4696067c6b65799
jonathanandrewbateeq <jonathan.andrew@bateeq.com>
2017-10-31 14:29:55 +0700

    Edit title penjualan kemarin (per produk)
    Edit title penjualan kemarin (per produk)

--- src/modules/daily-product-sales-product-1/index.js
+++ src/modules/daily-product-sales-product-1/index.js
@@ -1,7 +1,7 @@
 export class Index {
   configureRouter(config, router) {
     config.map([
-        {route:['', '/report'],       moduleId:'./report',          name:'report',        nav:false,      title:'Laporan Produk Harian'}
+        {route:['', '/report'],       moduleId:'./report',          name:'report',        nav:false,      title:'Laporan Produk Harian Kemarin'}
     ]); 
     this.router = router;
   }
