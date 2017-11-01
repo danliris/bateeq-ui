281dee32c507b7a15050da3965d9dd3c1d81fa02
edo-moonlay <31334028+edo-moonlay@users.noreply.github.com>
2017-11-01 16:49:01 +0700

    Update main.js

--- src/main.js
+++ src/main.js
@@ -25,6 +25,7 @@ export async function configure(aurelia) {
      var auth = "https://bateeq-auth-api-uat.mybluemix.net/v1/";
      var core = "https://bateeq-core-api-uat.mybluemix.net/v1/core/";
      var master = "https://bateeq-core-api-uat.mybluemix.net/v1/master/";
+     var manufacture = "https://bateeq-core-api-dev.mybluemix.net/v1/manufacture/";
      var inventory = "https://bateeq-inventory-api-uat.mybluemix.net/v1/inventory/";
      var merchandiser = "https://bateeq-inventory-api-uat.mybluemix.net/v1/merchandiser/";
      var sales = "https://bateeq-pos-api-uat.mybluemix.net/v1/sales/";
