92a456dce502b48be9712c8f9caa0ef532f7ebd6
stvyang-m <steven.yang@moonlay.com>
2017-11-10 18:50:59 +0700

    Add design tracking reason & modify design tracking
    Modification:
    1. Remove number of stage limiter
    2. Make 8 stage css color
    3. Add article counter

--- src/routes/designer.js
+++ src/routes/designer.js
@@ -1,5 +1,18 @@
 module.exports = [
     {
+        route: 'design-tracking-reason',
+        name: 'design-tracking-reason',
+        moduleId: './modules/design-tracking-reason/index',
+        nav: true,
+        title: 'Alasan Design Tracking',
+        auth: true,
+        settings: {
+            group: "designer",
+            permission: { "MRD.01": 1 },
+            iconClass: 'fa fa-adjust'
+        }
+    },
+    {
         route: 'design-tracking',
         name: 'design-tracking',
         moduleId: './modules/design-tracking/index',
