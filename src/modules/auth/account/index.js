dbc3c559c856f390f6871dc3591fbfd7e52fb9fe
AchmadSetiaji <achmad.setiaji@moonlay.com>
2017-11-20 10:38:43 +0700

    Enhancement: Change Password user account
    Change password for listed account and selected one to change password.

--- src/modules/auth/account/index.js
+++ src/modules/auth/account/index.js
@@ -4,7 +4,8 @@ export class Index {
             { route: ['', 'list'], moduleId: './list', name: 'list', nav: true, title: 'List' },
             { route: 'view/:id', moduleId: './view', name: 'view', nav: false, title: 'View:Account' },
             { route: 'edit/:id', moduleId: './edit', name: 'edit', nav: false, title: 'Edit:Account' },
-            { route: 'create', moduleId: './create', name: 'create', nav: false, title: 'Create:Account' }
+            { route: 'create', moduleId: './create', name: 'create', nav: false, title: 'Create:Account' },
+            { route: 'change-password/:id', moduleId: './change-password', name: 'changePassword', nav: false, title: 'Change:Password' }
         ]);
 
         this.router = router;
