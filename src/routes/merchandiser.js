04a9063e2e5531ed23cf4b7c6f1401f716148edf
edo-moonlay <31334028+edo-moonlay@users.noreply.github.com>
2018-01-10 01:07:39 +0700

    Update merchandiser.js

--- src/routes/merchandiser.js
+++ src/routes/merchandiser.js
@@ -38,56 +38,56 @@ module.exports = [
             iconClass: 'fa fa-university'
         }
     },
-    {
-        route: 'category',
-        name: 'category',
-        moduleId: './modules/merchandiser/category/index',
-        nav: true,
-        title: 'Kategori',
-        auth: true,
-        settings: {
-            group: "merchandiser",
-            permission: { "MRD.01": 1 },
-            iconClass: 'fa fa-adjust'
-        }
-    },
-    {
-        route: 'material',
-        name: 'material',
-        moduleId: './modules/merchandiser/material/index',
-        nav: true,
-        title: 'Bahan',
-        auth: true,
-        settings: {
-            group: "merchandiser",
-            permission: { "MRD.01": 1 },
-            iconClass: 'fa fa-adjust'
-        }
-    },
-    {
-        route: 'UOM',
-        name: 'UOM',
-        moduleId: './modules/merchandiser/UOM/index',
-        nav: true,
-        title: 'Satuan',
-        auth: true,
-        settings: {
-            group: "merchandiser",
-            permission: { "MRD.01": 1 },
-            iconClass: 'fa fa-adjust'
-        }
-    },
-    {
-        route: 'size',
-        name: 'size',
-        moduleId: './modules/merchandiser/size/index',
-        nav: true,
-        title: 'Ukuran',
-        auth: true,
-        settings: {
-            group: "merchandiser",
-            permission: { "MRD.01": 1 },
-            iconClass: 'fa fa-adjust'
-        }
-    }
+//    {
+//        route: 'category',
+//        name: 'category',
+//        moduleId: './modules/merchandiser/category/index',
+//        nav: true,
+//        title: 'Kategori',
+//        auth: true,
+//        settings: {
+//            group: "merchandiser",
+//            permission: { "MRD.01": 1 },
+//            iconClass: 'fa fa-adjust'
+//        }
+//    },
+ //   {
+ //       route: 'material',
+ //       name: 'material',
+ //       moduleId: './modules/merchandiser/material/index',
+ //       nav: true,
+ //       title: 'Bahan',
+ //       auth: true,
+ //       settings: {
+ //           group: "merchandiser",
+ //           permission: { "MRD.01": 1 },
+ //           iconClass: 'fa fa-adjust'
+ //       }
+ //   },
+//    {
+//        route: 'UOM',
+//        name: 'UOM',
+//        moduleId: './modules/merchandiser/UOM/index',
+//        nav: true,
+//        title: 'Satuan',
+//        auth: true,
+//        settings: {
+//            group: "merchandiser",
+//            permission: { "MRD.01": 1 },
+//            iconClass: 'fa fa-adjust'
+//        }
+//    },
+//    {
+//        route: 'size',
+//        name: 'size',
+//        moduleId: './modules/merchandiser/size/index',
+//        nav: true,
+//        title: 'Ukuran',
+//        auth: true,
+//        settings: {
+//            group: "merchandiser",
+//            permission: { "MRD.01": 1 },
+//            iconClass: 'fa fa-adjust'
+//        }
+//    }
 ]
