c7a1d35a23ce47a6d9dce0e7fcb150128d7a0b14
AchmadSetiaji <achmad.setiaji@moonlay.com>
2018-02-14 18:15:38 +0700

    update ui for unit receipt note

---
+++ src/modules/purchasing/unit-receipt-note/list/list.js
@@ -0,0 +1,83 @@
+import { inject } from 'aurelia-framework';
+import { Service } from "./../service";
+import { Router } from 'aurelia-router';
+var moment = require("moment");
+
+@inject(Router, Service)
+export class List {
+    columns = [
+        { field: "unitDivision", title: "Unit" },
+        { field: "no", title: "No. Bon Unit" },
+        {
+            field: "date", title: "Tanggal Bon Unit",
+            formatter: (value, data) => {
+                return moment(value).format("DD MMM YYYY");
+            }
+        },
+        { field: "supplier.name", title: "Supplier" },
+        { field: "deliveryOrder.no", title: "No. Surat Jalan" }
+    ];
+
+    context = ["Rincian", "Cetak PDF"];
+
+    today = new Date();
+
+    isPrint = false;
+
+    constructor(router, service) {
+        this.service = service;
+        this.router = router;
+    }
+
+    async activate() {
+
+    }
+
+    loader = (info) => {
+        var order = {};
+
+        if (info.sort)
+            order[info.sort] = info.order;
+
+        var arg = {
+            page: parseInt(info.offset / info.limit, 10) + 1,
+            size: info.limit,
+            keyword: info.search,
+            order: order
+        };
+
+        return this.service.search(arg)
+            .then(result => {
+                result.data.map((data) => {
+                    data.unitDivision = data.unit.division.name + " - " + data.unit.name;
+                    return data;
+                });
+
+                return {
+                    total: result.info.total,
+                    data: result.data
+                }
+            });
+    }
+
+    back() {
+        this.router.navigateToRoute('list');
+    }
+
+    create() {
+        this.router.navigateToRoute('create');
+    }
+
+    contextClickCallback(event) {
+        var arg = event.detail;
+        var data = arg.data;
+        switch (arg.name) {
+            case "Rincian":
+                this.router.navigateToRoute('view', { id: data._id });
+                break;
+            case "Cetak PDF":
+                this.service.getPdfById(data._id);
+                break;
+        }
+    }
+}
