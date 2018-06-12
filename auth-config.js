e1bf1bd45bc22a006c3564d4ae87c6b5afc6be54
novalinahtb <38346495+novalinahtb@users.noreply.github.com>
2018-06-12 17:32:04 +0700

    Add masterplan

--- auth-config.js
+++ auth-config.js
@@ -1,6 +1,6 @@
 export default {
     endpoint: "auth",
-    configureEndpoints: ["auth", "core", "master", "manufacture", "inventory", "merchandiser", "md", "sales", "purchasing"],
+    configureEndpoints: ["auth", "core", "master", "manufacture", "inventory", "merchandiser", "md", "sales", "purchasing", "masterplan"],
 
     loginUrl: "/authenticate",
     profileUrl: "/me",
