ad4661acacdec48330050a8b23dab2f18a2eaaef
novalinahtb <38346495+novalinahtb@users.noreply.github.com>
2018-07-23 11:48:40 +0700

    Update main.js

--- src/main.js
+++ src/main.js
@@ -21,29 +21,33 @@ export async function configure(aurelia) {
     .feature('components')
     .feature('converters')
     .plugin("aurelia-api", config => {
-
       var auth = "https://bateeq-auth-api-dev.azurewebsites.net/v1/";
       var core = "https://bateeq-core-api-dev.azurewebsites.net/v1/core/";
       var master = "https://bateeq-core-api-dev.azurewebsites.net/v1/master/";
-      var manufacture = "https://bateeq-core-api-dev.azurewebsites.net/v1/manufacture/";
-      var inventory = "https://bateeq-inventory-api-dev.azurewebsites.net/v1/inventory/";
-      var merchandiser = "https://bateeq-inventory-api-dev.azurewebsites.net/v1/merchandiser/";
-      var md = "https://com-bateeq-service-merchandiser-dev.azurewebsites.net/v1/";
+      var manufacture =
+        "https://bateeq-core-api-dev.azurewebsites.net/v1/manufacture/";
+      var inventory =
+        "https://bateeq-inventory-api-dev.azurewebsites.net/v1/inventory/";
+      var merchandiser =
+        "https://bateeq-inventory-api-dev.azurewebsites.net/v1/merchandiser/";
+      var md =
+        "https://com-bateeq-service-merchandiser-dev.azurewebsites.net/v1/";
       var sales = "https://bateeq-pos-api-dev.azurewebsites.net/v1/sales/";
-      var purchasing = "https://bateeq-inventory-api-dev.azurewebsites.net/v1/purchasing/";
-      var masterplan = "https://com-bateeq-service-masterplan-dev.azurewebsites.net/v1/";
-    
-      config.registerEndpoint('auth', auth);
-      config.registerEndpoint('core', core);
-      config.registerEndpoint('master', master);
-      config.registerEndpoint('manufacture', manufacture);
-      config.registerEndpoint('inventory', inventory);
-      config.registerEndpoint('merchandiser', merchandiser);
-      config.registerEndpoint('md', md);
-      config.registerEndpoint('sales', sales);
-      config.registerEndpoint('purchasing', purchasing);
-      config.registerEndpoint('masterplan', masterplan);
-    
+      var purchasing =
+        "https://bateeq-inventory-api-dev.azurewebsites.net/v1/purchasing/";
+      var masterplan =
+        "https://com-bateeq-service-masterplan-dev.azurewebsites.net/v1/";
+
+      config.registerEndpoint("auth", auth);
+      config.registerEndpoint("core", core);
+      config.registerEndpoint("master", master);
+      config.registerEndpoint("manufacture", manufacture);
+      config.registerEndpoint("inventory", inventory);
+      config.registerEndpoint("merchandiser", merchandiser);
+      config.registerEndpoint("md", md);
+      config.registerEndpoint("sales", sales);
+      config.registerEndpoint("purchasing", purchasing);
+      config.registerEndpoint("masterplan", masterplan);
     })
     .plugin("aurelia-authentication", baseConfig => {
       baseConfig.configure(authConfig);
