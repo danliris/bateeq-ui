253d364d69e5625dc36301e574159f452d58ff26
edo-moonlay <31334028+edo-moonlay@users.noreply.github.com>
2018-08-03 04:10:09 +0700

    Update main.js

--- src/main.js
+++ src/main.js
@@ -21,17 +21,23 @@ export async function configure(aurelia) {
     .feature('components')
     .feature('converters')
     .plugin("aurelia-api", config => {
+      var auth = "https://bateeq-auth-api-uat.azurewebsites.net/v1/";
+      var core = "https://bateeq-core-api-uat.azurewebsites.net/v1/core/";
+      var master = "https://bateeq-core-api-uat.azurewebsites.net/v1/master/";
+      var manufacture =
+        "https://bateeq-core-api-uat.azurewebsites.net/v1/manufacture/";
+      var inventory =
+        "https://bateeq-inventory-api-uat.azurewebsites.net/v1/inventory/";
+      var merchandiser =
+        "https://bateeq-inventory-api-uat.azurewebsites.net/v1/merchandiser/";
+      var md =
+        "https://com-bateeq-service-merchandiser-uat.azurewebsites.net/v1/";
+      var sales = "https://bateeq-pos-api-uat.azurewebsites.net/v1/sales/";
+      var purchasing =
+        "https://bateeq-inventory-api-uat.azurewebsites.net/v1/purchasing/";
+      var masterplan =
+        "https://com-bateeq-service-masterplan-uat.azurewebsites.net/v1/";
 
-      var auth = "https://bateeq-auth-api.azurewebsites.net/v1/";
-      var core = "https://bateeq-core-api.azurewebsites.net/v1/core/";
-      var master = "https://bateeq-core-api.azurewebsites.net/v1/master/";
-      var manufacture = "https://bateeq-core-api.azurewebsites.net/v1/manufacture/";
-      var inventory = "https://bateeq-inventory-api.azurewebsites.net/v1/inventory/";
-      var merchandiser = "https://bateeq-inventory-api.azurewebsites.net/v1/merchandiser/";
-      var md = "https://com-bateeq-service-merchandiser.azurewebsites.net/v1/";
-      var sales = "https://bateeq-pos-api.azurewebsites.net/v1/sales/";
-      var purchasing = "https://bateeq-inventory-api.azurewebsites.net/v1/purchasing/";
-      var masterplan = "https://com-bateeq-service-masterplan.azurewebsites.net/v1/";
     
       config.registerEndpoint('auth', auth);
       config.registerEndpoint('core', core);
