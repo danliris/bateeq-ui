6c975af27a1d4318639748c0080040a4de9927f4
AchmadSetiaji <achmad.setiaji@moonlay.com>
2018-09-11 13:35:37 +0700

    update for error validation

--- src/modules/master/discount/template/ro-item-details.js
+++ src/modules/master/discount/template/ro-item-details.js
@@ -47,7 +47,7 @@ export class ROItemDetails {
                 var formEnd = moment(innerData.endDate).endOf('day');
                 var itemStart = moment(dataItem.startDate).startOf('day');
                 var itemEnd = moment(dataItem.endDate).endOf('day');
-
+                debugger
                 if (innerData.discountOne == dataItem.discountOne &&
                     innerData.discountTwo == dataItem.discountTwo) {
 
@@ -55,15 +55,15 @@ export class ROItemDetails {
                         formStart <= itemEnd ||
                         itemStart >= formStart &&
                         itemStart <= formEnd) {
-                        item["error"] = "Produk sudah digunakan";
+                        errorCode = "Produk sudah digunakan";
                     }
                 } else {
-                    
+
                     if (formStart >= itemStart &&
                         formStart <= itemEnd ||
                         itemStart >= formStart &&
                         itemStart <= formEnd) {
-                        item["error"] = "Produk sudah digunakan";
+                        errorCode = "Produk sudah digunakan";
                     }
                 }
             });
