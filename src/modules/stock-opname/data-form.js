4ffa96b8ab9326b2c95f0543dd94528a3abf8112
Andri Sibarani <andri.sebastian@moonlay.com>
2018-11-28 11:29:19 +0700

    Stock Opname - Check All
    Stock Opname - Check All

--- src/modules/stock-opname/data-form.js
+++ src/modules/stock-opname/data-form.js
@@ -1,31 +1,42 @@
 import { bindable, inject, computedFrom } from "aurelia-framework";
 
-var moment = require('moment');
-
 
 export class DataForm {
     @bindable title;
     @bindable readOnly;
+    @bindable uploadDate;
 
     bind(context) {
         this.context = context;
         this.data = this.context.data;
         this.error = this.context.error;
-
+        
     }
 
     itemsColumns = [
         { header: "Barcode", value: "code" },
-        { header: "Nama Barang", value: "item.name" },
+        { header: "Nama Barang", value: "item.name"},
         { header: "Kuantitas Stock Sebelum SO", value: "qtyBeforeSO" },
         { header: "Kuantitas SO", value: "qtySO" },
         { header: "Selisih", value: "qtySO - qtyBeforeSO" },
-        { header: "Sesuaikan Dengan SO", value: "isAdjusted" },
+        { header: "Sesuaikan Dengan SO" , value: "isAdjusted"},
         { header: "catatan", value: "remark" }
     ]
 
     get status(){
         return this.data.isProcessed ? "Sudah Diproses" : "Belum Diproses";
+    }
+
+    get checkAllAdjusted(){
+        var changeElement = this.data.items.map(element => {
+            if (element.isEdit == true) {
+                element.isAdjusted = true;
+                element.remark = element._createdDate;
+            } 
+            return element;
+        });
+      
+        this.data.items = changeElement;
     }
 
 }
