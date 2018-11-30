a649a9923e6d6d8f086b57b7bc55776be4b8b621
AchmadSetiaji <achmad.setiaji@moonlay.com>
2018-11-30 15:36:22 +0700

    update ui for stockopname balance

--- src/modules/stock-opname/stock-opname-balance/check-balance.js
+++ src/modules/stock-opname/stock-opname-balance/check-balance.js
@@ -2,7 +2,6 @@ import { inject, Lazy } from 'aurelia-framework';
 import { Router } from 'aurelia-router';
 import { Service } from './service';
 var StorageLoader = require('../../../loader/storage-loader');
-var moment = require('moment');
 
 
 @inject(Router, Service, Element)
@@ -24,8 +23,6 @@ export class CheckBalance {
     checkBalance() {
         this.service.getBalanceByStorage(this.data.storage)
             .then((result) => {
-                console.log(result);
-                
                 this.stockOpnameBalance = result;
                 this.showTable = true;
             })
