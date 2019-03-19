d6b38c60af13222381d6554d71af318cdc76c4bd
Andi Budi <andibateeq@gmail.com>
2019-03-19 13:33:50 +0700

    Paging in Detail Aliran Stock
    Page in Detail Aliran Stock

--- src/modules/storage-inventory/view.js
+++ src/modules/storage-inventory/view.js
@@ -4,23 +4,51 @@ import { Service } from './service';
 
 @inject(Router, Service)
 export class View {
-
+    data = [];
+    info = { page: 1, keyword: '' };
+    params = {};
+    keyword = '';
     constructor(router, service) {
         this.router = router;
         this.service = service;
     }
 
-    activate(params) {
+
+    async activate(params) {
+        this.params = params;
+        console.log(params);  
+        this.info.keyword = '';
         var storageId = params.storageId;
         var itemId = params.itemId; 
-        this.service.getAllMovement(storageId, itemId)
-            .then(data => {
-                this.data = data;
+        var result = await this.service.getAllMovement(storageId, itemId, this.info);
+        this.data = result.data;
+        this.info = result.info;
+        var moment = require('moment');
+        for (var obj of this.data) {
+            obj.date = moment(obj.date, "YYYY-MM-DDTHH:mm:SSSZ").format("DD MMM YYYY - HH:mm:SS")
+        }
+    }
+
+   loadPage() {
+        var params = this.params;
+        var keyword = this.info.keyword;
+        var storageId = params.storageId;
+        var itemId = params.itemId; 
+        this.service.getAllMovement(storageId, itemId, this.info)
+            .then(result => {
+                this.data = result.data;
+                this.info = result.info;
                 var moment = require('moment');
                 for (var obj of this.data) {
                     obj.date = moment(obj.date, "YYYY-MM-DDTHH:mm:SSSZ").format("DD MMM YYYY - HH:mm:SS")
                 }
             })
+    }
+
+    changePage(e) {
+        var page = e.detail;
+        this.info.page = page;
+        this.loadPage();
     }
 
     list() {
