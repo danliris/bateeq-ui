aab6e0d8b85704c95e6114803e93ddecad7870f1
Andi Budi <andibateeq@gmail.com>
2019-10-21 09:15:23 +0700

    update EFR-KB-RTT

--- src/modules/efr-kb-rtt-cr/list.js
+++ src/modules/efr-kb-rtt-cr/list.js
@@ -32,9 +32,10 @@ export class List {
                 } 
              },
 
-             { field: "destination.code", title: "Tujuan",
+             { field: "tujuan", title: "Tujuan",
              formatter: function (value, row, index) {
-                return value ? "GDG.01 - GUDANG BARANG JADI 1" : "";
+                return `${value.code} - ${value.name}`;  
+               // return value ? "GDG.01 - GUDANG BARANG JADI 1" : "";
                 } 
               },
 
@@ -74,31 +75,51 @@ export class List {
             .then(result => {
                 var dataResult = result.data;
                 var dataSPKB = [];
+                let dataPLTS = [];
                 for(var a of dataResult){
                    dataSPKB.push(this.service.getSPKByReference(a.code));
+                   dataPLTS.push(this.service.getPackingListTransferStock(a.code));
                 }
                 return Promise.all(dataSPKB)
-                            .then(data => {
-                                for (var i = 0; i < dataResult.length; i++) {
-                                    for(var b = 0; b < data.length; b++){
-                                        if(data[b] != undefined && data[b].length > 0){
-                                            dataResult[i].spk = data[i][0];
+                    .then(data => {
+                       
+                        for (var i = 0; i < dataResult.length; i++) {
+                            for(var b = 0; b < data.length; b++){
+                                if(data[b] != undefined && data[b].length > 0){
+                                    dataResult[i].spk = data[i][0];
+                                }
+                            }
+                        }
+
+                        // for(var a of dataResult){
+                        //     for(var b = 0; b < data.length; b++){
+                        //         if(data[b] != undefined && data[b].length > 0){
+                        //             a.spk = data[b][0];
+                        //         }
+                        //     }
+                        // }
+
+                        return Promise.all(dataPLTS)
+                            .then(dataPLTS => {
+                                for (let i = 0; i < dataResult.length; i++) {
+                                    const packingListTransferStock = dataPLTS[i];
+                                    let tujuan;
+                                    if (packingListTransferStock) {
+                                        if (packingListTransferStock[0].source.code == packingListTransferStock[1].destination.code) {
+                                            tujuan = packingListTransferStock[0].source;
+                                        } else if (packingListTransferStock[0].destination.code == packingListTransferStock[1].source.code) {
+                                            tujuan = packingListTransferStock[0].destination;
                                         }
                                     }
+                                    dataResult[i].tujuan = tujuan;
                                 }
 
-                                // for(var a of dataResult){
-                                //     for(var b = 0; b < data.length; b++){
-                                //         if(data[b] != undefined && data[b].length > 0){
-                                //             a.spk = data[b][0];
-                                //         }
-                                //     }
-                                // }
                                 return {
                                     total: result.info.total,
                                     data: dataResult
                                 };
-                            });
+                            })
+                    });
             });
     }
 
