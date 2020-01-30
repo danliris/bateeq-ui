cd7bd894d4b3b313933eadcca7bb7507405b9849
Andi Budi <andibateeq@gmail.com>
2020-01-30 11:10:26 +0700

    add npurchasing module

---
+++ src/modules/npurchasing/purchase-order/template/purchase-order-item.js
@@ -0,0 +1,30 @@
+var ProductLoader = require('../../../../loader/nproduct-purchasing-loader');
+
+export class PurchaseOrderItem {
+  activate(context) {
+    console.log(context);
+    this.data = context.data;
+    this.error = context.error;
+    this.options = context.options;
+    // this.data.ProductRemark = this.data.remark;
+    if(this.data.quantity){
+      this.data.quantity=this.data.quantity.toLocaleString('en-EN', { minimumFractionDigits: 2 });
+    }
+  }
+
+  get productLoader() {
+    return ProductLoader;
+  }
+
+  productChanged(e) {
+    if (this.data.product)
+      this.data.productId = this.data.product._id ? this.data.product._id : {};
+      // this.data.ProductRemark = this.data.remark;
+  }
+
+  controlOptions = {
+    control: {
+      length: 12
+    }
+  };
+}
--- src/routes/nmaster.js
+++ src/routes/nmaster.js
@@ -13,6 +13,32 @@ module.exports = [
     }
 },
 {
+  route: 'suppliers/budgeting',
+  name: 'suppliers/budgeting',
+  moduleId: './modules/master/supplier-budgeting/index',
+  nav: true,
+  title: 'Supplier',
+  auth: true,
+  settings: {
+      group: "nmaster",
+      permission: { "C.01": 1, "PBL.01": 1 },
+      iconClass: 'fa fa-dashboard'
+  }
+},
+{
+  route: 'suppliers',
+  name: 'suppliers',
+  moduleId: './modules/master/supplier/index',
+  nav: true,
+  title: 'Supplier',
+  auth: true,
+  settings: {
+      group: "nmaster",
+      permission: { "C.01": 1, "PBL.01": 1 },
+      iconClass: 'fa fa-dashboard'
+  }
+},
+{
   route: 'divisions',
   name: 'divisions',
   moduleId: './modules/master/division/index',
@@ -61,7 +87,8 @@ module.exports = [
   auth: true,
   settings: {
       group: "nmaster",
-      permission: { "P1": 1, "P3": 1, "P4": 1, "P6": 1, "P7": 1, "S4": 1, "C3": 1, "E": 1, "K": 1, "S1": 1, "S2": 1, "S3": 1, "U1": 1, "F1": 1, "F2": 1, "L3": 1, "LK": 1, "L8": 1, "L2": 1, "C2": 1, "A2": 1, "C1": 1, "B5": 1, "L1": 1, "B4": 1, "B3": 1, "C4": 1, "OJ": 1, "A1": 1, "B9": 1, "A4": 1, "P1A": 1, "C2A": 1, "C2B": 1, "FP": 1, "PI": 1, "P": 1, "FC": 1, "GU": 1, "GS": 1, "PG": 1, "C1A": 1, "C1B": 1, "KK": 1, "B1": 1, "W1": 1, "W2": 1},
+      permission: {"C.01": 1, "MRD.01": 1 },
+     // permission: { "P1": 1, "P3": 1, "P4": 1, "P6": 1, "P7": 1, "S4": 1, "C3": 1, "E": 1, "K": 1, "S1": 1, "S2": 1, "S3": 1, "U1": 1, "F1": 1, "F2": 1, "L3": 1, "LK": 1, "L8": 1, "L2": 1, "C2": 1, "A2": 1, "C1": 1, "B5": 1, "L1": 1, "B4": 1, "B3": 1, "C4": 1, "OJ": 1, "A1": 1, "B9": 1, "A4": 1, "P1A": 1, "C2A": 1, "C2B": 1, "FP": 1, "PI": 1, "P": 1, "FC": 1, "GU": 1, "GS": 1, "PG": 1, "C1A": 1, "C1B": 1, "KK": 1, "B1": 1, "W1": 1, "W2": 1},
       iconClass: 'fa fa-dashboard'
   }
 },
