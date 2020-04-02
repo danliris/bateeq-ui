d9a092f2713ea194d487a71129e3da37da87bf8c
Andi Budi <andibateeq@gmail.com>
2020-04-02 11:37:58 +0700

    commit

--- src/routes/nmaster.js
+++ src/routes/nmaster.js
@@ -47,7 +47,7 @@ module.exports = [
   auth: true,
   settings: {
       group: "nmaster",
-      permission: { "C.01": 1, "PBL.01": 1, "C5": 1 },
+      permission: { "C.01": 1, "PBL.01": 1 },
       iconClass: 'fa fa-dashboard'
   }
 },
@@ -113,7 +113,7 @@ module.exports = [
   auth: true,
   settings: {
       group: "nmaster",
-      permission: {"C.01": 1, "MRD.01": 1, "PBA.01": 1, "PBF.01": 1, "PBL.01": 1, "C5": 1 },
+      permission: {"C.01": 1, "MRD.01": 1, "PBA.01": 1, "PBF.01": 1, "PBL.01": 1 },
       iconClass: 'fa fa-dashboard'
   }
 },
